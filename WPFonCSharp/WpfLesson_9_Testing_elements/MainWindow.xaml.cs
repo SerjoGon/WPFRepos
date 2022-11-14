ccaf600a272986eb3204dbb0ba6850af963857ea
mr.serhioru@mail.ru <mr.serhioru@mail.ru>
2022-11-14 21:19:44 +0300

    Start button

--- WPFonCSharp/WpfLesson_9_Testing_elements/MainWindow.xaml.cs
+++ WPFonCSharp/WpfLesson_9_Testing_elements/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,20 +25,34 @@ namespace WpfLesson_9_Testing_elements
         {
             InitializeComponent();
         }
-
         private void cb_firstitem_Click(object sender, RoutedEventArgs e)
         {
-
+            tree_sample.Items.Add(((ListBoxItem)lb_sample.Items[0]).Content);
         }
 
         private void cb_seconditem_Click(object sender, RoutedEventArgs e)
         {
-
+            TreeViewItem tree_sample2 = new TreeViewItem { Header = "Tree level 2"};
+            tree_sample2.Items.Add(((ListBoxItem)lb_sample.Items[1]).Content);
+            tree_sample2.Items.Add(((ListBoxItem)lb_sample.Items[2]).Content);
+            tree_sample.Items.Add(tree_sample2);
         }
 
         private void cb_thirditem_Click(object sender, RoutedEventArgs e)
         {
+            TreeViewItem tree_sample3 = new TreeViewItem { Header = "Tree level 3" };
+            tree_sample3.Items.Add(((ListBoxItem)lb_sample.Items[3]).Content);
+            tree_sample3.Items.Add(((ListBoxItem)lb_sample.Items[4]).Content);
+            ((TreeViewItem)tree_sample.Items[1]).Items.Add(tree_sample3);
+        }
 
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            for(double i = Pb.Minimum; i < Pb.Maximum;i++)
+            {
+                Form.Height = i;
+                Pb.Value = i;
+            }
         }
     }
 }
